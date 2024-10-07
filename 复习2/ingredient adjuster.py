d22b641b90d4951f111f892d574976595541bc0a
Ariel Geo Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2024-10-07 16:34:13 +0800

    Delete 复习2 directory

--- 复习2/ingredient adjuster.py
+++
@@ -1,9 +0,0 @@
-#A cookie recipe calls for the following ingredients:
-#• 1.5 cups of sugar
-#• 1 cup of butter
-#• 2.75 cups of flour
-#The recipe produces 48 cookies with this amount of the ingredients.
-#Write a program that asks the user how many cookies he or she wants to make, and then displays
-#the number of cups of each ingredient needed for the specified number of cookies.
-cookieamount=int(input("How many cookies do you want? >>>"))
-print("You need",cookieamount*1.5/48,"cups of sugar,",cookieamount/48,"cups of butter and",cookieamount*2.75/48,"cups of flour")
--- 复习2/sales prediction.py
+++
@@ -1,6 +0,0 @@
-#A company has determined that its annual profit is typically 23 percent of total sales.
-#Write a program that asks the user to enter the projected amount of total sales, and then displays
-#the profit that will be made from that amount.
-a=float(input("What is your total sales?>>>"))
-b=a*0.23
-print("Your profit is",b)
