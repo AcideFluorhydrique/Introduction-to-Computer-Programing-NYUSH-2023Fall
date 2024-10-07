d2ebc43ba0de8a07e698f3c6ada8661d8f6117aa
Ariel Geo Wang <139669368+AcideFluorhydrique@users.noreply.github.com>
2024-10-07 16:35:09 +0800

    Delete 复习4 directory

--- 复习4/(Gaddis 4.14) Pattern 2 (NL).py
+++
@@ -1,3 +0,0 @@
-n = 6
-for i in range(n):
-    print("#{}#".format(" "*3*i))
--- 复习4/Password Generator.py
+++
@@ -1,6 +0,0 @@
-import random
-x = int(input("Password size? > "))
-y=1
-while y <= x:
-    print(random.randint(0,9),end="")
-    y = y + 1
